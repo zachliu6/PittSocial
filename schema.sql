3e6133914f39b6fff557dd2c0ba6c7e1d0a9e3d6
Connor Allington <connorallington13@gmail.com>
2019-11-07 14:49:04 -0500

    Foreign Keys

--- schema.sql
+++ schema.sql
@@ -2,14 +2,18 @@ CREATE TABLE groupMember(
     gID integer,
     userID integer,
     role varchar(20),
-    CONSTRAINT groupMember_pk PRIMARY KEY (gID, userID)
+    PRIMARY KEY (gID, userID),
+    FOREIGN KEY (gID) REFERENCES group(gID),
+    FOREIGN KEY (userID) REFERENCES profile(userID)
 );
 
 CREATE TABLE pendingGroupMember(
     gID integer,
     userID integer,
     message varchar(200),
-    CONSTRAINT pendingGroupMember_pk PRIMARY KEY (gID, userID)
+    PRIMARY KEY (gID, userID),
+    FOREIGN KEY (gID) REFERENCES group(gID),
+    FOREIGN KEY (userID) REFERENCES profile(userID)
 );
 
 create table profile
