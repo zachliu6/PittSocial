5e5991f9d2ce0297a863a03c77bc129791b50e27
Zach Liu <zachliu0406@gmail.com>
2019-11-06 20:36:32 -0500

    zach

--- schema.sql
+++ schema.sql
@@ -11,3 +11,29 @@ CREATE TABLE pendingGroupMember(
     message varchar(200),
     CONSTRAINT pendingGroupMember_pk PRIMARY KEY (gID, userID)
 );
+
+create table profile
+(
+    userID  int,
+    name    varchar(50),
+    email   varchar(50),
+    password varchar(50),
+    date_of_birth   date,
+    lastlogin   timestamp,
+    Constraint PROFILE_PK primary key (userID)
+);
+
+create table friend(
+    userID1  int,
+    userID2  int,
+    JDate    date,
+    message  varchar(200),
+    Constraint friend_PK primary key (userID1, userID2)
+);
+
+create table pendingFiend(
+    fromID    int,
+    toID      int,
+    message varchar(200),
+    CONSTRAINT pendingFiend_PK primary key (fromID, toID)
+);
