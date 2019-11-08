e34a27880325d2c7393f1a2d02c7a352ae863516
Zach Liu <zachliu0406@gmail.com>
2019-11-08 18:11:54 -0500

    insert data

---
+++ insert.sql
@@ -0,0 +1,122 @@
+INSERT INTO profile
+VALUES (100, 'Zach', '123@gmail.com', '123456', 4-6-1999, 11-7-2019);
+INSERT INTO profile
+VALUES (101, 'Jason', '123@gmail.com', '123456', 4-6-1999, 11-7-2019);
+INSERT INTO profile
+VALUES (102, 'Pete', '123@gmail.com', '123456', 4-6-1999, 11-7-2019);
+INSERT INTO profile
+VALUES (103, 'Jake', '123@gmail.com', '123456', 4-6-1999, 11-7-2019);
+INSERT INTO profile
+VALUES (104, 'Cole', '123@gmail.com', '123456', 4-6-1999, 11-7-2019);
+INSERT INTO profile
+VALUES (105, 'Mike', '123@gmail.com', '123456', 4-6-1999, 11-7-2019);
+INSERT INTO profile
+VALUES (106, 'Luke', '123@gmail.com', '123456', 4-6-1999, 11-7-2019);
+INSERT INTO profile
+VALUES (107, 'Connor', '123@gmail.com', '123456', 4-6-1999, 11-7-2019);
+INSERT INTO profile
+VALUES (108, 'Justin', '123@gmail.com', '123456', 4-6-1999, 11-7-2019);
+INSERT INTO profile
+VALUES (109, 'Ross', '123@gmail.com', '123456', 4-6-1999, 11-7-2019);
+INSERT INTO profile
+VALUES (110, 'Olivia', '123@gmail.com', '123456', 4-6-1999, 11-7-2019);
+INSERT INTO profile
+VALUES (111, 'Riya', '123@gmail.com', '123456', 4-6-1999, 11-7-2019);
+INSERT INTO profile
+VALUES (112, 'Arthur', '123@gmail.com', '123456', 4-6-1999, 11-7-2019);
+INSERT INTO profile
+VALUES (113, 'Jared', '123@gmail.com', '123456', 4-6-1999, 11-7-2019);
+INSERT INTO profile
+VALUES (114, 'Emily', '123@gmail.com', '123456', 4-6-1999, 11-7-2019);
+INSERT INTO profile
+VALUES (115, 'catherine', '123@gmail.com', '123456', 4-6-1999, 11-7-2019);
+INSERT INTO profile
+VALUES (116, 'Sabrina', '123@gmail.com', '123456', 4-6-1999, 11-7-2019);
+INSERT INTO profile
+VALUES (117, 'Matt', '123@gmail.com', '123456', 4-6-1999, 11-7-2019);
+INSERT INTO profile
+VALUES (118, 'Colin', '123@gmail.com', '123456', 4-6-1999, 11-7-2019);
+INSERT INTO profile
+VALUES (119, 'John', '123@gmail.com', '123456', 4-6-1999, 11-7-2019);
+INSERT INTO profile
+VALUES (120, 'Joseph', '123@gmail.com', '123456', 4-6-1999, 11-7-2019);
+INSERT INTO profile
+VALUES (121, 'Cameron', '123@gmail.com', '123456', 4-6-1999, 11-7-2019);
+INSERT INTO profile
+VALUES (122, 'Evan', '123@gmail.com', '123456', 4-6-1999, 11-7-2019);
+INSERT INTO profile
+VALUES (123, 'Devon', '123@gmail.com', '123456', 4-6-1999, 11-7-2019);
+INSERT INTO profile
+VALUES (124, 'Nate', '123@gmail.com', '123456', 4-6-1999, 11-7-2019);
+INSERT INTO profile
+VALUES (125, 'Robbert', '123@gmail.com', '123456', 4-6-1999, 11-7-2019);
+INSERT INTO profile
+VALUES (126, 'Ari', '123@gmail.com', '123456', 4-6-1999, 11-7-2019);
+INSERT INTO profile
+VALUES (127, 'Ben', '123@gmail.com', '123456', 4-6-1999, 11-7-2019);
+INSERT INTO profile
+VALUES (128, 'Jack', '123@gmail.com', '123456', 4-6-1999, 11-7-2019);
+INSERT INTO profile
+VALUES (129, 'Owen', '123@gmail.com', '123456', 4-6-1999, 11-7-2019);
+INSERT INTO profile
+VALUES (130, 'Nick', '123@gmail.com', '123456', 4-6-1999, 11-7-2019);
+INSERT INTO profile
+VALUES (131, 'Gregory', '123@gmail.com', '123456', 4-6-1999, 11-7-2019);
+INSERT INTO profile
+VALUES (132, 'Zack', '123@gmail.com', '123456', 4-6-1999, 11-7-2019);
+INSERT INTO profile
+VALUES (133, 'Gabby', '123@gmail.com', '123456', 4-6-1999, 11-7-2019);
+INSERT INTO profile
+VALUES (134, 'Ryan', '123@gmail.com', '123456', 4-6-1999, 11-7-2019);
+INSERT INTO profile
+VALUES (135, 'Mackenzie', '123@gmail.com', '123456', 4-6-1999, 11-7-2019);
+INSERT INTO profile
+VALUES (136, 'Emma', '123@gmail.com', '123456', 4-6-1999, 11-7-2019);
+INSERT INTO friend
+VALUES ('Zach', 'Pete',11-1-2019, 'Hello');
+INSERT INTO friend
+VALUES ('Zach', 'Jake',11-1-2019, 'Hello');
+INSERT INTO friend
+VALUES ('Zach', 'Owen',11-1-2019, 'Hello');
+INSERT INTO friend
+VALUES ('Zach', 'Jack',11-1-2019, 'Hello');
+INSERT INTO friend
+VALUES ('Pete', 'Ari',11-1-2019, 'Hello');
+INSERT INTO friend
+VALUES ('Emily', 'Pete',11-1-2019, 'Hello');
+INSERT INTO friend
+VALUES ('Mackenzie', 'Pete',11-1-2019, 'Hello');
+INSERT INTO friend
+VALUES ('Cameron', 'Pete',11-1-2019, 'Hello');
+INSERT INTO friend
+VALUES ('Joesph', 'Pete',11-1-2019, 'Hello');
+INSERT INTO friend
+VALUES ('Connor', 'Pete',11-1-2019, 'Hello');
+INSERT INTO friend
+VALUES ('Jack', 'Pete',11-1-2019, 'Hello');
+INSERT INTO friend
+VALUES ('Owen', 'Pete',11-1-2019, 'Hello');
+INSERT INTO friend
+VALUES ('Emma', 'Pete',11-1-2019, 'Hello');
+INSERT into "group"
+values (1, 'Nordenberg', 20, 'pitt');
+INSERT into "group"
+values (2, 'Sutherland', 20, 'pitt');
+INSERT into "group"
+values (3, 'Holland', 20, 'pitt');
+INSERT into "group"
+values (4, 'Oakland', 20, 'pitt');
+INSERT into "group"
+values (5, 'Cathy', 20, 'pitt');
+INSERT into "group"
+values (6, 'Towers', 20, 'pitt');
+INSERT into "group"
+values (7, 'Panther Hall', 20, 'pitt');
+INSERT into messages
+values (10, 100, 'Hello', NULL, 10-31-2019);
+INSERT into messages
+values (11, 101, 'Hello', NULL, 10-31-2019);
+INSERT into messages
+values (12, 102, 'Hello', NULL, 10-31-2019);
+INSERT into messages
+values (13, 103, 'Hello', NULL, 10-31-2019);
